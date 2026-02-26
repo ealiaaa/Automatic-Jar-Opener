98e2026cd8aef063f672d6dfc443d698ca90d1eb
raykna <raykna@proton.me>
2026-02-25 21:18:30 -0500

    uploaded changes made on raspberry pi and shortened printStatus to be only 1 print statement

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 import time, sys
 from gpiozero import Motor,  LED
-from sensor_library import Force_Sensing_Resistor # recommended to import *
+from sensor_library import Force_Sensing_Resistor
+
 
 '''
 Constants
@@ -16,7 +17,7 @@ but 0.1 isnt fast enough to calculate real-time slippage at a reasonable rate, a
 to solve this, each tick is subdivided into 10 subticks for the rolling lists for average and average variation ->
 to be completed refreshed every tick
 '''
-REQUIRED_HANDLE_FORCE = 50 # required sustained grip value to detect grip on handle
+REQUIRED_HANDLE_FORCE = 100 # required sustained grip value to detect grip on handle
 SAMPLE_WINDOW_LEN = 10  # number of samples per window (~0.1s rn)
 MAX_ACCEPTABLE_VARIATION = 1  
 # num of slips that need to happen in a row to confirm that it's *actually* slipping and not a sensor fluke
@@ -64,9 +65,9 @@ PERHAPS
 '''
 Initialize force sensors, motors and LEDs
 '''
-sensorTop = Force_Sensing_Resistor(0)
-sensor1 = Force_Sensing_Resistor(1)
-sensor2 = Force_Sensing_Resistor(2)
+sensorTop = Force_Sensing_Resistor(1)
+sensor1 = Force_Sensing_Resistor(2)
+sensor2 = Force_Sensing_Resistor(3)
 
 
 motorGrab = Motor(forward = 16, backward=20)
@@ -76,7 +77,7 @@ red = LED(26)
 green = LED(19)
 
 '''
-Function Definitions 
+Sensor-based and helper Functions
 '''
 
 def readSensor(sensor): #returns raw sensor value of passed sensor when called, delay is implemented in func tick()
@@ -89,55 +90,13 @@ def readyStart (): # waits infinity until topSensor detects hand for more than 0
         while readSensor(sensorTop) > REQUIRED_HANDLE_FORCE:
             heldCounter += 1
             time.sleep(TICK_PERIOD)
-            if heldCounter >= 5:
+            if heldCounter >= 10:
                 print("Starting lid grip sequence")
                 return
 
 
-def shouldAbort (): # exits straight to finally clause if user lets go of handle for more than 5 times in a row
-    global consecutiveGripValuesFailed
-
-    if readSensor(sensorTop) < REQUIRED_HANDLE_FORCE:
-        consecutiveGripValuesFailed += 1
-        if consecutiveGripValuesFailed >= 7: 
-            raise SystemExit
-        else:
-            return
-# 5 because 1 would be susceptible to sensor variations, but 7 is still short enough to allow for quick recognition of handle release
-    else:
-        consecutiveGripValuesFailed = 0
-        return
-
-
-def jarGrabbed(grabSpeed, grabTo): # tightens grabbing belt until grabTo value, returns if grabTo Value reached yet or not
-    global timeGrabbed
-    global triedGrab
-
-    if rollingAverage < grabTo:
-        motorGrab.forward(grabSpeed)
-        timeGrabbed += TICK_PERIOD*grabSpeed
-        triedGrab = grabSpeed
-        return False
-    return True
-
-
-def twist(twistSpeed): # twists entire jar lid grip mechanism open and returns whether lid is open or not
-    global timeTwisted
-    global triedTwist
-
-    motorTwist.forward(twistSpeed)
-    timeTwisted += TICK_PERIOD*twistSpeed
-    triedTwist = twistSpeed
-
-    if timeTwisted > 9: # 9 is the currect ESTIMATE of how long itll take t
-        print("Lid sucessfully opened")
-        return True
-    return False
-
 def printStatuses():
-    print("---")
-    print(f"Top Force Sensor: {readSensor(sensorTop)}\tJar Force Sensor1: {readSensor(sensor1)}\tJar Force Sensor2:  {readSensor(sensor2)}")
-    print(f"Red LED: {redState}\tGreen LED: {greenState}\tGrabbing Motor Speed: {triedGrab}\tTwisting Motor Speed: {triedTwist}")
+    print(f"---znTop Force Sensor: {readSensor(sensorTop)}\tJar Force Sensor1: {readSensor(sensor1)}\tJar Force Sensor2:  {readSensor(sensor2)}\nRed LED: {redState}\tGreen LED: {greenState}\tGrabbing Motor Speed: {triedGrab}\tTwisting Motor Speed: {triedTwist}")
 
 
 def dropProgram(motor, duration): # reverses motors back to initial positions exactly
@@ -164,7 +123,6 @@ abs() could also do that but **2 or **4 or a bigger number also amplifies smalle
 
 def rollingForceAverage12(): #rolling average of bottom 2 force sensors
     global rollingAverage # used to communicate with jarGrabbed to enable motor and sensor call isolation
-
     sensor1RecentAvg.append( readSensor(sensor1) )
     sensor2RecentAvg.append( readSensor(sensor2) )
 
@@ -179,7 +137,7 @@ def rollingForceAverage12(): #rolling average of bottom 2 force sensors
     
     rollingAverage = (avg1+avg2)/2
 '''
-i would love to break slipDetect and rollingForceAverage12 into 1 function to collect the rolling window, and 2 functinos to process them
+I would like to break slipDetect and rollingForceAverage12 into 1 function to collect the rolling window, and 2 functinos to process them
 but the rubric specifically wants the rolling Average to be ONE functions only.
 '''
 
@@ -207,10 +165,27 @@ def slipDetect (): #calculates rolling **variation** of bottom 2 sensors, determ
         grabSpeed = 1
         grabTo = 200
         twistSpeed = 1
+##        print("SLIPPING")
     else:
         grabSpeed = 0.5
         grabTo = 150
         twistSpeed = 0.5
+##        print("NOT slipping")
+
+def shouldAbort (): # exits straight to finally clause if user lets go of handle for more than 5 times in a row
+    global consecutiveGripValuesFailed
+    a = readSensor(sensorTop)
+    b = readSensor(sensorTop)
+    if b < REQUIRED_HANDLE_FORCE:
+        consecutiveGripValuesFailed += 1
+        if consecutiveGripValuesFailed >= 10:
+            raise SystemExit
+        else:
+            return
+# 5 because 1 would be susceptible to sensor variations, but 7 is still short enough to allow for quick recognition of handle release
+    else:
+        consecutiveGripValuesFailed = 0
+        return
 
 
 def sensorTick():
@@ -218,18 +193,48 @@ def sensorTick():
     rollingForceAverage12()
     shouldAbort()
 
+'''
+Motor-based functions
+'''
+def jarGrabbed(grabSpeed, grabTo): # tightens grabbing belt until grabTo value, returns if grabTo Value reached yet or not
+    global timeGrabbed
+    global triedGrab
+
+    if rollingAverage < grabTo:
+        motorGrab.forward(grabSpeed)
+        timeGrabbed += 10*TICK_PERIOD*grabSpeed
+        triedGrab = grabSpeed
+        return False
+    return True
+
+
+def twist(twistSpeed): # twists entire jar lid grip mechanism open and returns whether lid is open or not
+    global timeTwisted
+    global triedTwist
+
+    motorTwist.forward(twistSpeed)
+    timeTwisted += 10*TICK_PERIOD*twistSpeed
+    triedTwist = twistSpeed
+
+    if timeTwisted > 0.1: # 9 is the currect ESTIMATE of how long itll take t
+        print("-----/nLid sucessfully opened/n------")
+        return #True
+    return False
 
 def motorTick():
     if not jarGrabbed(grabSpeed, grabTo):
         red.off() # flashes red when only trying to grip the lid
         redState = "Flashing"
+##        print("gripping lid")
         return False
     
     if twist(twistSpeed):
         return True
+##    print("twisting lid")
     red.on()
     redState = "ON"
     return False
+
 
 
 def main():
@@ -240,39 +245,43 @@ def main():
     print("Waiting for user to grip...")
     green.on()
     greenState = "ON"
+
     readyStart()  # waits until it detects user holding handle for more than 0.5s in a row
+    
     greenState = "Flashing"
+    
     next_tick = time.monotonic() # sets next_tick to a set large, steadily increasing number that is INDEPENDANT of compute time.
+    
     opened = False
     while not opened:
         next_tick += TICK_PERIOD
+        tickCounter += 1
 
         sensorTick()
         
         if tickCounter == 50: # every 50 ticks, will trigger print function ( once every 0.5 seconds)
-            printStatuses()
+            #printStatuses()
             triedGrab = 0
             triedTwist = 0
-            tickCounter = 0
-        if tickCounter % 20 == 0 and tickCounter != 0:
-            green.off() # flashes green every 0.1 seconds while program running, green turns on in line 258
-            red.on() # solid red only when motorTick is trying to twist the lid
         if tickCounter % 10 == 0 and tickCounter != 0: # every 10 ticks will trigger motors to update their condition (motors cant on/off faster than 0.1 s)
             motorGrab.stop()
             motorTwist.stop()
             green.on()
             opened = motorTick()  # will return True for the program to exit (should abort will exit direcctly with exit())
+        if tickCounter % 20 == 0 and tickCounter != 0:
+            green.off() # flashes green every 0.1 seconds while program running, green turns on in line 258
+            red.on() # solid red only when motorTick is trying to twist the lid
 
         if tickCounter >= 100: # resets tickCounter every second (100 ticks)
             tickCounter = 0
-        
-        tickCounter += 1
-        
+        ##print(tickCounter)
+            
+##        time.sleep(TICK_PERIOD)
         sleep_time = next_tick-time.monotonic() # calculates how long to sleep for to keep tick rates exactly consistent considering compute time
         if sleep_time > 0:
             time.sleep(sleep_time)
-
-        green.on()
+            
+        
         
 
 
@@ -281,9 +290,10 @@ try:
 finally:
     green.on()
     red.on()
+    motorGrab.stop()
+    motorTwist.stop()
     greenState = "ON"
     redState= "ON"
-    printStatuses()
     print("Retracting Twisting motor to original position")
     dropProgram(motorTwist, timeTwisted)
     print("Retracting Grabbing motor to original position")
@@ -291,6 +301,10 @@ finally:
     print("Program done")
     green.off()
     red.off()
+    greenState = "OFF"
+    redState = "OFF"
+    printStatuses()
+    print(timeGrabbed,timeTwisted)
 
 
 '''
